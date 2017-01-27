1694a26ca1156e8f3fcf1befd5e16fd49f5d964a
Terry McKee <javalion@hotmail.com>
2017-01-27 07:59:20 -0600

    Support for Dynamic Other Job Role Field

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
+"use strict";
 // JavaScript Document
 
 function initialize() {
 
+  // Setup Dynamic Fields
+  setupDynamicFields();
+	
+  // Setup Event Handlers
+  setupEventHandlers();
 }
+
+function setupDynamicFields() {
+    // Setup Other Job Role Field
+	var $otherJobRole = $('<label for="other-title">Other:</label> <input type="text" id="other-title" placeholder="Your Job Role">');
+	$otherJobRole.hide();
+	$('#title').after($otherJobRole);
+	
+}
+
+function setupEventHandlers() {
+	$('#title').on('change',handleJobRoleChange);
+}
+
+//
+// Event Handlers
+//
+function handleJobRoleChange(){
+	if (this.value === 'other')
+	{
+       // Show Other Job Role Field		
+	   $('#other-title').show();
+	}
+	else 
+	{
+	   // Hide Other Job Role Field
+		$('#other-title').hide();
+	}
+
+}
+
 
 initialize();
